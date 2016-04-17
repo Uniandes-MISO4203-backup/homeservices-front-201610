03d7c340dd9e6e442b21b0744ee8b677612f87e0
jgarcia100uniandes <j.garcia100@uniandes.edu.co>
2016-04-17 18:53:43 -0500

    R9 finished

--- app/src/modules/serviceRequestPrices/serviceRequestPrices.ctrl.js
+++ app/src/modules/serviceRequestPrices/serviceRequestPrices.ctrl.js
@@ -3,9 +3,13 @@
             .controller('serviceRequestPricesCtrl', ['$scope','Restangular',
             'serviceRequestContext', 'serviceRequestPriceContext','$state',
         function ($scope, Restangular, parentUrl, url,$state) {
+            $scope.currentDate = new Date();
             $scope.serviceRequestId = $state.params.serviceRequestId;
             $scope.fetchData = function () {
-                $scope.serviceRequest = Restangular.one(parentUrl, $scope.serviceRequestId).get();
+                $scope.serviceRequest = Restangular.one(parentUrl, $scope.serviceRequestId).get().then(function (object) {
+                    $scope.serviceRequest = object;
+                    $scope.priceRequestLimit = new Date(object.priceRequestLimit);
+                });
                 $scope.data = Restangular.one(parentUrl, $scope.serviceRequestId).getList(url);
                 /* for testing */
                 $scope.data = [{
