0ea7729afbb0d0d81463dbc745b94249f2f03f46
jgarcia100uniandes <j.garcia100@uniandes.edu.co>
2016-05-09 18:09:44 -0500

    Technical debt

---
+++ e2e/specs/authentication_customer.specs.js
@@ -0,0 +1,33 @@
+describe('Authentication contractor E2E Testing', function () {
+	browser.driver.manage().window().maximize();        
+    
+    beforeEach(function () {
+        browser.addMockModule('ngCrudMock', function () {
+            var mod = angular.module('ngCrudMock');
+
+            mod.run(["$httpBackend",
+                function($httpBackend){
+                    baseUrl='api';
+                    var statisticURL = new RegExp(baseUrl + '/(\\w+)/(\\w+)');
+                    var authentication = {"email":"contractor_ciclo3@saberes.com","givenName":"Contractor",
+                        "middleName":"cICLO","rememberMe":false,
+                        "roles":["customer"],"surName":"Ciclo 3",
+                        "userName":"contractor_ciclo3"};
+                    $httpBackend.whenGET(statisticURL).respond(function (method, url) {
+                        console.warn('GET skills');
+                        return [200, authentication, {}];
+                    });
+                }
+            ]);
+            mod.run(['ngCrudMock.mockRecords', function(records){
+                records['serviceRequests'] = [];
+            }]);
+        });
+    });
+
+    it('authentication contractor', function () {       
+        browser.get('#/catalog');
+        expect(element.all(by.css('ul#menu li')).first().getText()).toEqual('Customer');
+    });
+});
+
---
+++ e2e/specs/authentication_null.specs.js
@@ -0,0 +1,29 @@
+describe('Authentication contractor E2E Testing', function () {
+	browser.driver.manage().window().maximize();        
+    
+    beforeEach(function () {
+        browser.addMockModule('ngCrudMock', function () {
+            var mod = angular.module('ngCrudMock');
+
+            mod.run(["$httpBackend",
+                function($httpBackend){
+                    baseUrl='api';
+                    var statisticURL = new RegExp(baseUrl + '/(\\w+)/(\\w+)');
+                    $httpBackend.whenGET(statisticURL).respond(function (method, url) {
+                        console.warn('GET skills');
+                        return [200, null, {}];
+                    });
+                }
+            ]);
+            mod.run(['ngCrudMock.mockRecords', function(records){
+                records['serviceRequests'] = [];
+            }]);
+        });
+    });
+
+    it('authentication contractor', function () {       
+        browser.get('#/catalog');
+        expect(element.all(by.css('ul#menu li')).count()).toEqual(0);
+    });
+});
+
