2afcdd609af1282e675e0e210961dd7a18837516
jgarcia100uniandes <j.garcia100@uniandes.edu.co>
2016-05-09 09:58:24 -0500

    R13 push

--- app/src/modules/priceRequest/priceRequest.ctrl.js
+++ app/src/modules/priceRequest/priceRequest.ctrl.js
@@ -1,7 +1,8 @@
 (function (ng) {
     var mod = ng.module('priceRequestModule', ['restangular']);
 
-    mod.controller('priceRequestCtrl', ['$scope', 'Restangular','$state', function ($scope, Restangular, $state) {
+    mod.controller('priceRequestCtrl', ['$scope', 'Restangular','$state','$modal',
+        function ($scope, Restangular, $state, $modal) {
         $scope.priceResquest = Restangular.all('priceRequests').getList().$object;
 
         $scope.rejectPriceRequest = function (x) {
@@ -21,18 +22,25 @@
         };
         $scope.goChat = function (idCustomer, idContractor) {
             $state.go('chat', {chatName: "CU" + idCustomer + "CO" + idContractor});
-
         };
+        $scope.score=function(serviceRequest){
+            $modal.open({
+                templateUrl: "src/modules/priceRequest/customerReview.tpl.html",
+                resolve: {serviceRequest: serviceRequest},
+                controller: 'customerReviewModalShowCtrl', controllerAs: 'ctrl'
+            });
+        }
     }]);
     mod.controller('customerReviewModalShowCtrl', ['$scope', 'Restangular','$state', '$window','$modalInstance','serviceRequest',
         function ($scope, Restangular, $state, $window, $modalInstance, serviceRequest) {
         $scope.serviceRequest = serviceRequest;
-        
-        $scope.ok = function () {
-            $modalInstance.close();
-        };
-        $scope.getTimes = function (n) {
-            return new Array(n);
+        $scope.value='3';
+
+        $scope.sendReview = function () {
+            Restangular.all("reviews").post({name: $scope.name, value: $scope.value, customer: serviceRequest.customer}).
+            then(function () {
+                $modalInstance.close();
+            });
         };
     }]);
 
